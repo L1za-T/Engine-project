7b6195ba944d70407a07f2ee12b1ea532e99ec89
L1za-T <78148912+L1za-T@users.noreply.github.com>
2023-12-16 23:40:13 +0000

    Added map files, added resource depository

---
+++ jsGameEngine/resources/Depository, do not use directly/grassland tileset/Grassland tileset 64.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Grassland tileset 64" tilewidth="64" tileheight="64" tilecount="4" columns="2">
+ <image source="../Depository, do not use directly/Grasslands - Free/tileset-grassland-grass.png" width="160" height="160"/>
+</tileset>
